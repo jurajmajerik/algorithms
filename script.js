af340894365eddce5698d743f1c8e1dc5285a86b
jurajmajerik <juro.majerik@gmail.com>
2018-01-23 11:33:25 +0700

    Title Case a Sentence

--- script.js
+++ script.js
@@ -14,6 +14,36 @@ document.addEventListener("DOMContentLoaded", function(event) {
     longestWordOutput.innerHTML = winner;
   });
 
+  // Title Case a Sentence
+  let titlecaseButton = document.getElementById("titlecase-button");
+  titlecaseButton.addEventListener('click', function() {
+    let titlecaseInput = document.getElementById("titlecase-input").value;
+    let titlecaseOutput = document.getElementById("titlecase-output");
+    let result = "";
+    // Split str into array of words
+    let myArr = titlecaseInput.split(" ");
+    // Outer Loop through the words
+    for (let i = 0; i < myArr.length; i++) {
+      // Inner Loop through the letters
+      for (let j = 0; j < myArr[i].length; j++) {
+        // If j=0 -> Capitalize the letter, then add to result
+        if (j === 0) {
+          result += myArr[i][j].toUpperCase();
+          // If i!=0 -> Lowercase the letter, add to result
+        } else {
+          result += myArr[i][j].toLowerCase();
+        }
+      }
+      // End of Inner Loop - add a whitespace
+      result += " ";
+    }
+    // End of Outer Loop - remove last whitespace
+    let myArr2 = result.split(" ");
+    myArr2.pop();
+    result = myArr2.join(" ");
+    titlecaseOutput.innerHTML = result;
+  });
+
 
 
 
